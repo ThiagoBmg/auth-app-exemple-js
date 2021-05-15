31a2817175c0defd2fdbcb4398158eb328dd5638
Thiago <th.bmg5274@gmail.com>
2021-05-15 18:53:00 -0300

    sistema funcionando

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,34 +1,45 @@
 var usersData = []; // declarando a variavel que ira guardar os usuários cadastrados durante o tempo de execução
 
 const auth = (user, password) =>{
+
     if(!user||user == undefined || !password || password == undefined) return false; // validando os valores
+
     var searchUser = usersData.filter((x) =>{if(x.username == user)return true})[0]; // procurando pelo usuário no objeto principal de usuários
+
     //console.log(searchUser)
     if(!searchUser|| searchUser == []) return false; // caso o retorno da pesquisa pelo nome do usuário for falso, ele retorna falso. 
+
     //console.log(searchUser.password)
     if(searchUser.password == password)  return true; // validando senha depois de ter validado o email
+
     return false; // caso não tiver retornado o valor verdadeiro nas condições anteriores, só resta retornar false (não autenticado) 
 };
 
 const createUser = (user, password ) =>{
+
     if(!user||user == undefined || !password || password == undefined) return {status:0, msg: 'valores fornecidos são invalidos para executar este processo'}; // validando os valores
-    //console.log(usersData.filter((x)=>{return x.username == user})[0])
+  
     if(!usersData.filter((x)=>{return x.username == user}) == undefined) return {status:0, msg: 'usuário já existente'}; // validando se o usuário já existe na base
+
     usersData.push({username: user, password: password }); // incluindo o novo usuário base de usuários
+
     return  {status:1, msg: 'usuário criado com sucesso'}; // retornando que o usuário foi criado com sucesso
+
 };
 
 module.exports = {
     create(req,res){
-        var user = req.query.user || null;
+        //console.log(req.query.username)
+        var user = req.query.username || null;
         var pass = req.query.password || null;
         var response = createUser(user,pass);
+        if(response.status) return res.render('sucesso');
         return res.send(response.msg);
     },
     login(req,res){
-        var user = req.query.user || null;
+        var user = req.query.username || null;
         var pass = req.query.password || null;
         if(!auth(user,pass)) return res.status(400).render('ops');
-        return res.status(200).send({status:'autenticated'});
+        return res.status(200).render('home');
     }
 };
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const controller = require('../controller/users');
 
-router.post('/cad-new-user', controller.create);
+router.get('/cad-new-user', controller.create);
 
-router.post('/auth',controller.login);
+router.get('/auth',controller.login);
 
 module.exports = router;
